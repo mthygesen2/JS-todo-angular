274fce9ba864764fac9a521815a160380659c171
Mao Thygesen <maothygesen@gmail.com>
2016-03-22 15:22:22 -0700

    add form

---
+++ app/task-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskComponent } from './task.component';
+import { Task } from './task.model';
+import { EditTaskDetailsComponent } from './edit-task-details.component';
+
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
+  directives: [TaskComponent, EditTaskDetailsComponent],
+  templateUrl: 'app/task-list.component.html'
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
