a2378787e7bbdb8c6609b28cdba4b35291672aba
Mao Thygesen <maothygesen@gmail.com>
2016-03-22 16:35:36 -0700

    finished homework

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,12 +2,14 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
-import { NewTaskComponent} from './new-task.component';
+import { NewTaskComponent } from './new-task.component';
+import {DonePipe} from './done.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
+  pipes: [DonePipe],
   directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   templateUrl: 'app/task-list.component.html'
 })
@@ -15,17 +17,20 @@ export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
   taskClicked(clickedTask: Task): void {
-    console.log('child', clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
   }
   createTask(description: string): void {
-  this.taskList.push(
-    new Task(description, this.taskList.length)
-  );
-}
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
+  }
+  onChange(filterOption) {
+    this.filterDone = filterOption;
+  }
 }
